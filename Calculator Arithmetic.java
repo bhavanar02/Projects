6aa42455e259c0401de14c4c0f7593c216f52808
Bhavana Raja <127816986+bhavanar02@users.noreply.github.com>
2024-03-15 23:59:22 -0700

    Add files via upload

---
+++ Calculator Arithmetic.java
@@ -0,0 +1,22 @@
+public class BasicCalculator {
+
+    public final double Add(double x, double y){
+        return x + y;
+    }
+
+    public final double Subtract(double x, double y){
+        return x - y;
+    }
+
+    public final double multiply(double x, double y){
+        return x * y;
+    }
+
+    public final double divide(double x, double y){
+        return x / y;
+    }
+
+    public final double modulo(double x, double y){
+        return x % y;
+    }
+}
