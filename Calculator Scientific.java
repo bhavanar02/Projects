86fde7bd2a2c58f80071416d1e2c76e7910d3e20
Bhavana Raja <127816986+bhavanar02@users.noreply.github.com>
2024-03-15 23:59:40 -0700

    Add files via upload

---
+++ Calculator Scientific.java
@@ -0,0 +1,10 @@
+public class ScientificCalculator {
+
+    public final double Exp(double x){
+        return Math.exp(x);
+    }
+
+    public final double Log(double x){
+        return Math.log(x);
+    }
+}
