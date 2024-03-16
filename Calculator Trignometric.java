c025710dca9f47241e0fe57e9be8dff04380779b
Bhavana Raja <127816986+bhavanar02@users.noreply.github.com>
2024-03-15 23:59:56 -0700

    Add files via upload

---
+++ Calculator Trignometric.java
@@ -0,0 +1,27 @@
+public class TrigonometricCalculator {
+
+    public final double Sine(double x){
+        return Math.sin(x);
+    }
+    
+    public final double Cosine(double x){
+        return Math.cos(x);
+    }
+    
+    public final double Tangent(double x){
+        return Math.tan(x);
+    }
+    
+    public final double Arcsine(double x){
+        return Math.asin(x);
+    }
+    public final double Arccosine(double x){
+        return Math.acos(x);
+    }
+    
+    public final double Arctangent(double x){
+        return Math.atan(x);
+    }
+    
+    
+}
